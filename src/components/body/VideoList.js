2e607dfd695a71e130ddcdbce21d8ba9804cf192
Aman Garg <amangarg24071995@gmail.com>
2024-10-04 18:19:26 +0530

    get the data from youtube API and show the data in log

--- src/components/body/VideoList.js
+++ src/components/body/VideoList.js
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react";
+import { YOUTUBE_API } from "../../utils/images/Constant";
+import VideoCard from "./VideoCard";
 const VideoList = () =>{
+    const [data,setData] = useState();
+    const getVideos = async () =>{
+        const data = await fetch(YOUTUBE_API);
+        const json = await data.json();
+        console.log(json.items);
+        setData(json.items);
+    }
+    useEffect(()=>{getVideos()},[])    
     return(
         <div>
-            <h1>Video List</h1>
+            <VideoCard info={data[0]}/>
         </div>
     )
 }
---
+++ src/utils/images/Constant.js
@@ -0,0 +1,4 @@
+const API_KEY = "AIzaSyBQTWeVllc2h0n_mz1KF2Rx_zdc43PJdV4";
+
+export const YOUTUBE_API = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key="+API_KEY;
+
